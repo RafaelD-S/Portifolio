00b60959922c67a0b488d0cb26b102f123d70cdc
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2023-12-05 17:16:18 -0300

    Conhecimento section passada para styled-components

--- src/components/conhecimento.jsx
+++ src/components/conhecimento.jsx
@@ -1,3 +1,4 @@
+import { styled } from "styled-components"
 import { useState } from "react"
 import Git from '../assets/git.png'
 import Github from '../assets/github.png'
@@ -7,9 +8,53 @@ import Js from '../assets/js.png'
 import React from '../assets/react.png'
 import Sass from '../assets/sass.png'
 import Figma from '../assets/figma.png'
-import Styled from '../assets/styled-components.png'
+import StyledImg from '../assets/styled-components.png'
 
 import {Fade} from 'react-awesome-reveal'
+
+const ConhecimentoSection = styled.section `
+
+    background-color: #ff8c002b;
+    border-block: 3px solid #2125291e;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 5rem 1.5rem;
+    color: #212529;
+    text-align: center;
+
+    article {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        gap: 5px;
+        max-width: 65rem;
+        width: 100%;
+    }
+`
+const AreaCards = styled.section `
+
+    padding-top: 1rem;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    gap: 10px 0;
+
+    img {
+        width: 24px;
+    }
+    div {
+        background-color: #fff;
+        display: flex;
+        padding: 1rem;
+        gap: 0 10px;
+        box-shadow: 0 0 5px gray;
+        border-radius: 5px;
+        max-width: 30rem;
+        width: 100%;
+        margin: auto;
+    }
+`
 
 
 export default function Conhecimento() {
@@ -45,7 +90,7 @@ export default function Conhecimento() {
         },
         {
             nome: 'Styled Components',
-            imagem: Styled
+            imagem: StyledImg
         },
         {
             nome: 'Figma',
@@ -53,7 +98,7 @@ export default function Conhecimento() {
         }
     ])
     return (
-        <section className="conhecimento" id="conhecimento">
+        <ConhecimentoSection className="conhecimento" id="conhecimento">
             <article>
                 <Fade cascade direction="up" duration={500} damping={.25} triggerOnce fraction={.1}>
                 <h2>
@@ -62,16 +107,16 @@ export default function Conhecimento() {
                 <p>
                     Saiba todas as tecnologias e ferramentas nas quais eu tenho conhecimento!
                 </p>
-                    <div className='area-cards'>
+                    <AreaCards className='area-cards'>
                             {cards.map((e) => (
                                 <div className="card">
                                     <img src={e.imagem} alt="" />   
                                     <h3>{e.nome}</h3>
                                 </div>
                             ))}
-                    </div>
+                    </AreaCards>
                 </Fade>
             </article>
-        </section>
+        </ConhecimentoSection>
     )
 }
