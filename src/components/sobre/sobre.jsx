c993f437d8619bc08e11a02aef62f3b1349c189c
Rafael Dantas <113727583+RafaelD-S@users.noreply.github.com>
2024-02-21 12:33:18 -0300

    Atualizar sobre.jsx

--- src/components/sobre/sobre.jsx
+++ src/components/sobre/sobre.jsx
@@ -13,7 +13,7 @@ export default function Sobre() {
                         Quem eu sou?
                     </h2>
                     <p>
-                    Olá, me chamo Rafael e atualmente sou estudante de programação Front-End junior através da instituição Vai na Web e em um futuro próximo aspiro me formar em análise e desenvolvimento de sistemas pela Universidade Federal da Bahia &#40;UFBA&#41;.
+                    Olá, me chamo Rafael e sou programador Front-End, atualmente no caminho de tecnólogo de Análise e Desenvolvimento de Sistemas no IFBA.
                     </p>
                     <p>
                     Sou bastante comunicativo e social, tenho boas capacidades de trabalho em equipe e liderança, sempre disposto a conhecer novas pessoas, novos ambientes e novas tecnologias, amo utilizar da minha criatividade em meus projetos e meu amor pela programação começou desde que aprendi HTML pela primeira vês, e me motiva a aprender mais e mais todos os dias.
